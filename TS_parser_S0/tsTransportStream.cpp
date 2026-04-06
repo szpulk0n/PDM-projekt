b2263452fe7fb4eccc292982a59ea0bdec5a68ee
szpulk0n <konrad.szpulecki@gmail.com>
2026-04-06 20:14:45 +0200

    Initial commit parser

---
+++ TS_parser_S0/tsTransportStream.cpp
@@ -0,0 +1,31 @@
+#include "tsTransportStream.h"
+
+//=============================================================================================================================================================================
+// xTS_PacketHeader
+//=============================================================================================================================================================================
+
+
+/// @brief Reset - reset all TS packet header fields
+void xTS_PacketHeader::Reset()
+{
+  //write me
+}
+
+/**
+  @brief Parse all TS packet header fields
+  @param Input is pointer to buffer containing TS packet
+  @return Number of parsed bytes (4 on success, -1 on failure) 
+ */
+int32_t xTS_PacketHeader::Parse(const uint8_t* Input)
+{
+  //i`m empty
+  return NOT_VALID;
+}
+
+/// @brief Print all TS packet header fields
+void xTS_PacketHeader::Print() const
+{
+  //print sth
+}
+
+//=============================================================================================================================================================================
