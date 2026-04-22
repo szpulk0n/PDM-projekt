7a487e3e0db821cd03fabe94f3f022784ab4baaf
szpulk0n <konrad.szpulecki@gmail.com>
2026-04-22 10:31:08 +0200

    Commit from lab

---
+++ TS_parser_S0/build/CMakeFiles/TS-PARSER.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lab/Documents/PDM-projekt/TS_parser_S0/TS_parser.cpp" "CMakeFiles/TS-PARSER.dir/TS_parser.cpp.o" "gcc" "CMakeFiles/TS-PARSER.dir/TS_parser.cpp.o.d"
+  "/home/lab/Documents/PDM-projekt/TS_parser_S0/tsTransportStream.cpp" "CMakeFiles/TS-PARSER.dir/tsTransportStream.cpp.o" "gcc" "CMakeFiles/TS-PARSER.dir/tsTransportStream.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
